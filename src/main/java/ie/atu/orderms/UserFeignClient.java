d40e207c4b7ef6997bd4e1523697d5432ed348ba
cian murray <g00358011@atu.ie>
2024-01-10 10:36:17 +0000

    openfeign added and tested

--- src/main/java/ie/atu/orderms/UserFeignClient.java
+++ src/main/java/ie/atu/orderms/UserFeignClient.java
@@ -5,12 +5,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "userms")
+@FeignClient(name = "userms", url = "http://localhost:8082")
 public interface UserFeignClient {
 
     @GetMapping("/user/{customerId}")
     User getUserByCustomerId(@PathVariable String customerId);
 
-    @GetMapping("/menu/{itemId}")
-    String getMenuItemById(@PathVariable String itemId);
+    //@GetMapping("/menu/{itemId}")
+    //String getMenuItemById(@PathVariable String itemId);
 }
